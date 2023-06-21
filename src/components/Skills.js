6110f22c472c166847ed5059869982e3cebe0403
raden muhammad renaldy <muhammadaldy2016@gmail.com>
2023-06-21 12:42:37 +0700

    delete orm in pages skill

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -46,7 +46,7 @@ const Skills = () => {
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Sequelize} alt="HTML icon" />
-                  <p className='my-4'>SEQUELIZE ORM</p>
+                  <p className='my-4'>SEQUELIZE</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Ejs} alt="HTML icon" />
